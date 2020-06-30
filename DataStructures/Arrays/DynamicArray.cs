7abbdef41ef0f6e21cb9c9593cba14cc9d24f8a5
CainSakura <cain@iloli.cc>
2020-07-01 00:09:18 +0800

    Improved comments and code robustness.

--- DataStructures/Arrays/DynamicArray.cs
+++ DataStructures/Arrays/DynamicArray.cs
@@ -6,13 +6,15 @@ namespace DataStructures.Arrays
 {
     public class DynamicArray<T>
     {
-        private T[] arr;
-        private int len = 0;
-        private int capacity = 0;
+        private T[] arr;            //泛型数组
+        private int len = 0;        //数组当前长度
+        private int capacity = 0;   //动态数组总容量
         public DynamicArray()
         {
+            // 默认初始数组长度为 16
             Create(16);
         }
+        // 创建动态数组，参数为数组容量
         public void Create(int capacity)
         {
             if (capacity < 0)
@@ -20,22 +22,35 @@ namespace DataStructures.Arrays
             this.capacity = capacity;
             arr = new T[capacity];
         }
+        // 获取数组当前长度
         public int Size()
         {
             return len;
         }
+        // 判断动态数组当前是否为空
         public bool IsEmpty()
         {
             return Size() == 0;
         }
+        // 获取指定索引位置的元素
         public T Get(int index)
         {
+            if (index >= len && index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
             return arr[index];
         }
+        // 设置指定索引位置的元素值
         public void Set(int index, T elem)
         {
+            if (index >= len && index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
             arr[index] = elem;
         }
+        // 清空动态数组
         public void Clear()
         {
             for (int i = 0; i < capacity; i++)
@@ -44,8 +59,10 @@ namespace DataStructures.Arrays
             }
             len = 0;
         }
+        // 添加元素
         public void Add(T elem)
         {
+            // 判断动态数组如果满了，则进行扩容
             if (len + 1 >= capacity)
             {
                 if (capacity == 0)
@@ -63,8 +80,10 @@ namespace DataStructures.Arrays
                 }
                 arr = newArr;
             }
+            // 在动态数组末尾添加元素
             arr[len++] = elem;
         }
+        // 移除指定索引位置的元素，返回删除元素的值
         public T RemoveAt(int rm_index)
         {
             if (rm_index >= len && rm_index < 0)
@@ -88,6 +107,7 @@ namespace DataStructures.Arrays
             capacity = --len;
             return data;
         }
+        // 移除数组内指定元素，返回布尔值判断是否成功
         public bool Remove(T item)
         {
             for (int i = 0; i < len; i++)
@@ -100,6 +120,7 @@ namespace DataStructures.Arrays
             }
             return false;
         }
+        // 获取指定元素在动态数组中的索引
         public int IndexOf(T item)
         {
             for (int i = 0; i < len; i++)
@@ -111,6 +132,7 @@ namespace DataStructures.Arrays
             }
             return -1;
         }
+        // 判断动态数组中是否包含指定元素
         public bool Contains(T item)
         {
             return IndexOf(item) != -1;
